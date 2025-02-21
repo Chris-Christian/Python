506e41bad4eff76106c8de07df9a5535c8792555
Chris Christian <realmdragon35@gmail.com>
2025-02-21 20:33:54 +0530

    Update and rename  Strong Number (sum of factorial of digits=input).py to Strong number (sum of factorial of digits=input).py

--- Strong Number/Strong number (sum of factorial of digits=input).py
+++ Strong Number/Strong number (sum of factorial of digits=input).py
@@ -19,25 +19,6 @@ if(real_user_input==sum_of_digits):
     print(f"{real_user_input} is a Strong number")
 else:
     print(f"{real_user_input} is not a Strong number")
-
-
-#Range of Strong number
-import math
-N=int(input("Enter N : "))
-for i in range(1,N+1):
-    user_input=i
-    real_user_input=i
-    count=0
-    while(i!=0):
-        last_digit=i%10
-        i=i//10
-        count+=1
-    i = user_input
-    flag=count
-    sum_of_digits=0
-    while(flag!=0):
-        digit=i%10
-        sum_of_digits += math.factorial(digit)
         i=i//10
         flag-=1
 
