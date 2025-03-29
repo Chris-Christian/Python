fd5196c4bc41069c3bc2bef826fd33a6b8811827
Chris Christian <realmdragon35@gmail.com>
2025-03-29 19:43:57 +0530

    Update and rename Session-1_print.py to Session-1_user_input and type_casting.py

--- Session-1_user_input and type_casting.py
+++ Session-1_user_input and type_casting.py
@@ -4,3 +4,8 @@ print(f"My name is {Name} and my favourite number is {fav_number}")
 
 age=int(input(print("Enter your age")))
 print(f"Your age is {age}")
+age=str(age)
+print(age)
+
+name=bool(Name) #used to check if the variable is assigned any value
+print(name) #True as output
