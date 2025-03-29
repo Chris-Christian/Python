12e5c5386f15f5c5e0ff045b5f637bc2c269d760
Chris Christian <realmdragon35@gmail.com>
2025-03-29 19:59:08 +0530

    Create Session-2_math.py

---
+++ Session-2_math.py
@@ -0,0 +1,16 @@
+friends=2
+#friends+=1
+# friends-=1
+# friends*=1
+# friends/=1
+# friends**=1
+remainder=friends%3
+print(remainder)
+
+x=3.14
+y=-4
+z=5
+result=round(x) #3
+result=abs(y) #4
+result=pow(4,3) #64
+result=max(x,y,z) #z
