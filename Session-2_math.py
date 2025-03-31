5f5a95afe5c0b33b0722ab49a70ba4178223748f
Chris Christian <realmdragon35@gmail.com>
2025-03-31 17:38:46 +0530

    Update Session-2_math.py

--- Session-2_math.py
+++ Session-2_math.py
@@ -1,3 +1,5 @@
+import math
+
 friends=2
 #friends+=1
 # friends-=1
@@ -14,3 +16,11 @@ result=round(x) #3
 result=abs(y) #4
 result=pow(4,3) #64
 result=max(x,y,z) #z
+
+print(math.pi)
+print(math.e)
+result=math.sqrt(x)
+result=math.ceil(x) #round up floating numbers
+result=math.floor(x) #round down floating numbers
+result=round(x,2) #round till 2 decimals
+
