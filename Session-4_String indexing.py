29fe1653b65758ab1d292367ffec0a1cb9d0b333
Chris Christian <realmdragon35@gmail.com>
2025-04-03 20:18:59 +0530

    Create Session-4_String indexing.py

---
+++ Session-4_String indexing.py
@@ -0,0 +1,8 @@
+# [Start:End:Step]
+Credit_number="1234-5678-1234-5678"
+print(Credit_number[0])
+print(Credit_number[:4])
+print(Credit_number[5:9])
+print(Credit_number[5:])
+print(Credit_number[-1])
+print(Credit_number[::2]) #Count every 2nd character #Steps can be negative too
