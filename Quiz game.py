f34999ae316536bc6e880555859cef75e0560e6a
Chris Christian <realmdragon35@gmail.com>
2025-08-27 19:22:30 +0530

    Update Quiz game.py

--- Quiz game.py
+++ Quiz game.py
@@ -1,12 +1,14 @@
 questions=("Which is the closest planet to the Sun in our Solar system? ",
           "Which is the hottest planet in our Solar system? ",
           "Which living being lays the largest eggs? ",
-          "Which is the largest ocean on planet Earth? ")
+          "Which is the largest ocean on planet Earth? ",
+          "How many continents are there in the world? ")
 options=(("A. Earth", "B. Venus", "C. Mars", "D. Mercury"),
          ("A. Mercury", "B. Venus", "C. Jupiter", "D. Mars"),
          ("A. Blue Whale", "B. Ostrich", "C. Falcon", "D. Pigeon"),
-         ("A. Indian", "B. Pacific", "C. Atlantic", "D. Arctic"))
-answers=("D","B","B","B")
+         ("A. Indian", "B. Pacific", "C. Atlantic", "D. Arctic"),
+         ("A. 6", "B. 8", "C. 7", "D. 9"))
+answers=("D","B","B","B","C")
 guesses=[]
 score=0
 question_num=0
@@ -33,3 +35,13 @@ print("-----------------------------")
 print(f"Your Total score is: {score}")
 results=(score/question_num)*100
 print(f"Percentage: {results}%")
+if results>80:
+    print("Excellent!")
+elif results>60:
+    print("Very Good!")
+elif results>40:
+    print("Good")
+else:
+    print("Keep practicing")
+print(f"Your choices: {guesses}")
+print(f"Correct answers: {answers}")
