cc3a46ec137e117f232141d4c43e7ce49c35a100
Chris Christian <realmdragon35@gmail.com>
2025-09-11 17:14:24 +0530

    Add files via upload

---
+++ Session-18_membership operators.py
@@ -0,0 +1,10 @@
+# Membership operators = used to test whether a value or variable is found in a sequence
+#                          (string, list, tuple, set or dictionary)
+#                          1. in
+#                          2. not in
+
+email="chris@email.com"
+if "@" in email and "." in email:
+    print("Valid email")
+else:
+    print("Invalid email")
