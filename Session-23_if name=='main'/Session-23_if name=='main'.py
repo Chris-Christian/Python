3184653a415cfe37737e0ea7e393a55b84c2effe
Chris Christian <realmdragon35@gmail.com>
2025-09-19 15:55:41 +0530

    Add files via upload

---
+++ Session-23_if name=='main'/Session-23_if name=='main'.py
@@ -0,0 +1,13 @@
+# if __name__==__main__: (this script can be imported or run standalone)
+#                         functions and classes in this module acn be reused
+#                         without the main block of code executing
+
+# ex. library=import library for functionality
+#             when running library directly, display a help page
+
+def main():
+    #program      #only run main code if we are running it directly
+    pass
+
+if __name__=='__main__':
+    main()
