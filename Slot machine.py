58aa06097606a1041917a883ce458269c0c847df
Chris Christian <realmdragon35@gmail.com>
2025-09-20 23:22:28 +0530

    Add files via upload

---
+++ Slot machine.py
@@ -0,0 +1,63 @@
+import random
+
+def spin_row():
+    symbols=["🥭","🍎","🍒","🔔","⭐"]
+    
+    results=[]
+    return [random.choice(symbols) for symbol in range(3)] #list comprehension
+def print_row(row):
+    print(" | ".join(row))
+
+def get_payout(row, bet):
+    if row[0]==row[1]==row[2]:
+        if row[0]=='🍒':
+            return bet*3
+        elif row[0]=='🍎':
+            return bet*4
+        elif row[0]=='🥭':
+            return bet*5
+        elif row[0]=='🔔':
+            return bet*10
+        elif row[0]=='⭐':
+            return bet*15
+    return 0
+def main():
+    balance=100
+    print("*************************")
+    print("Welcome to slot machine  ")
+    print("Symbols: 🍒 🍎 🥭 🔔 ⭐")
+    print("*************************")
+
+    while balance>0:
+        print(f"Current balance: {balance}")
+
+        bet=input("Place your bet amount: ")
+        if not bet.isdigit():
+            print("Please enter a valid number")
+            continue
+        bet=int(bet)
+        if bet>balance:
+            print("Insufficient funds")
+            continue
+        elif bet<=0:
+            print("Bet amount must be greater than 0")
+            continue
+        balance-=bet
+
+        row=spin_row()
+        print("Spinning...\n")
+        print_row(row)
+
+        payout=get_payout(row, bet)
+        if payout>0:
+            print(f"You won: {payout}Rs")
+        else:
+            print("Sorry you lost this round")
+        balance+=payout
+
+        play_again = input("Do you want to spin again? (Y to continue, any other key to exit): ").upper()
+        if play_again!='Y':
+            break
+    print(f"Game over! Your final balance is {balance}")
+if __name__=='__main__':
+    main()
